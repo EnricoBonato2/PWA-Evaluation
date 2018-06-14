f1100b7cd4525551abf352cb1ae57f0e2fb57921
Unknown <enrico.bonato.5@studenti.unipd.it>
2018-06-14 13:25:44 +0200

    separato codice js

---
+++ script.js
@@ -0,0 +1,27 @@
+function Controller($scope, $http) {
+  $scope.getInfo = function() {
+    $scope.compAndLoc = false;
+    $scope.compOrLoc = false;
+    $scope.bio = false;
+    $scope.webSite = false;
+    $scope.userNotFound = false;
+    $scope.noUser = true;
+    $scope.userFound = false;
+    $http
+      .get("https://api.github.com/users/" + $scope.username)
+      .success(function(data) {
+        if (data.name == "") data.name = data.login;
+        if (data.company != null && data.location != null)
+          $scope.compAndLoc = true;
+        if (data.company != null || data.location != null)
+          $scope.compOrLoc = true;
+        if (data.bio == "") $scope.bio = true;
+        if (data.blog == "") $scope.webSite = true;
+        $scope.user = data;
+        $scope.userFound = true;
+      })
+      .error(function() {
+        $scope.userNotFound = true;
+      });
+  };
+}
